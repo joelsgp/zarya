85951bdab2d3b9c3e6daaef9f3debfe7d9096dd5
JMcB#7918 <joel.mcbride1@live.com>
2021-01-15 16:01:35 +0000

    Write sum docstrings

--- discord_funcs.py
+++ discord_funcs.py
@@ -4,10 +4,15 @@ import random
 import discord
 
 
-# TODO: write docstrings
-
-
 async def discord_stutter(text, channel, delay=lambda: random.randint(1, 3)/100, skip=False):
+    """Send a message to a discord channel, with gradual print effect.
+
+    Args:
+        text -- the message to send
+        channel -- discord channel object to send it to
+        delay -- a function that returns the time in seconds to wait between each character
+        skip -- if True, message will be sent all at once instead of with the gradual effect
+    """
     if skip:
         await channel.send(text)
     else:
@@ -18,6 +23,16 @@ async def discord_stutter(text, channel, delay=lambda: random.randint(1, 3)/100,
 
 
 def input_from_message(message, channel, prefixes=None):
+    """Get input from a discord message.
+
+    Args:
+        message -- discord message object
+        channel -- discord channel to pay attention to messages in, all channels if this is False
+        prefixes -- list of command prefixes
+    Returns:
+        The message with the prefix removed. The return value will be an empty string if conditions were not met,
+        which means this function can be used to check validity and to strip prefixes.
+    """
     if not prefixes:
         prefixes = ['>', '9v']
 
@@ -30,6 +45,16 @@ def input_from_message(message, channel, prefixes=None):
 
 
 async def discord_input(client, channel, prefixes=None):
+    """Wait for a discord message with valid input, then return it.
+
+    Like the builtin input() function but for discord
+    Args:
+        client -- discord client object
+        channel -- discord channel to pay attention to messages in
+        prefixes -- list of command prefixes
+    Returns:
+        String content from a valid message, with the prefix removed.
+    """
     while True:
         new_message = await client.wait_for('message')
         check = input_from_message(new_message, channel, prefixes)
@@ -38,4 +63,5 @@ async def discord_input(client, channel, prefixes=None):
 
 
 def send_logs(channel, path='log.txt'):
+    """Send logs to a discord channel."""
     channel.send(discord.File(path))
